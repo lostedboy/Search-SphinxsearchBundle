7c7029a3c4ec2016a2d60f986361d07b31d53248
emgiezet <maxmalecki@gmail.com>
2013-05-14 11:04:22 +0200

    namespace fix - psr fixes

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -8,61 +8,79 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-	/**
-	 * Generates the configuration tree.
-	 *
-	 * @return TreeBuilder
-	 */
-	public function getConfigTreeBuilder()
-	{
-		$treeBuilder = new TreeBuilder();
-		$rootNode = $treeBuilder->root('sphinxsearch');
+    /**
+     * Generates the configuration tree.
+     *
+     * @return TreeBuilder
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('sphinxsearch');
 
-		$this->addIndexerSection($rootNode);
-		$this->addIndexesSection($rootNode);
-		$this->addSearchdSection($rootNode);
+        $this->addIndexerSection($rootNode);
+        $this->addIndexesSection($rootNode);
+        $this->addSearchdSection($rootNode);
 
-		return $treeBuilder;
-	}
-
-	private function addIndexerSection(ArrayNodeDefinition $node)
-	{
-		$node
-			->children()
-				->arrayNode('indexer')
-					->addDefaultsIfNotSet()
-					->children()
-						->scalarNode('bin')->defaultValue('/usr/bin/indexer')->end()
-					->end()
-				->end()
-			->end();
-	}
-
-	private function addIndexesSection(ArrayNodeDefinition $node)
-	{
-		$node
-			->children()
-				->arrayNode('indexes')
-					->isRequired()
-					->requiresAtLeastOneElement()
-					->useAttributeAsKey('key')
-					->prototype('scalar')->end()
-				->end()
-			->end();
-	}
-
-	private function addSearchdSection(ArrayNodeDefinition $node)
-	{
-		$node
-			->children()
-				->arrayNode('searchd')
-					->addDefaultsIfNotSet()
-					->children()
-						->scalarNode('host')->defaultValue('localhost')->end()
-						->scalarNode('port')->defaultValue('9312')->end()
-						->scalarNode('socket')->defaultNull()->end()
-					->end()
-				->end()
-			->end();
-	}
+        return $treeBuilder;
+    }
+    /**
+     * Adding indexer to config
+     * 
+     * @param ArrayNodeDefinition $node the root node
+     * 
+     * @return null
+     */
+    protected function addIndexerSection(ArrayNodeDefinition $node)
+    {
+        $node
+            ->children()
+            ->arrayNode('indexer')
+            ->addDefaultsIfNotSet()
+            ->children()
+            ->scalarNode('bin')->defaultValue('/usr/bin/indexer')->end()
+            ->end()
+            ->end()
+            ->end();
+    }
+    /**
+     * Adding indexes to config
+     * 
+     * @param ArrayNodeDefinition $node the root node
+     * 
+     * @return null
+     */
+    protected function addIndexesSection(ArrayNodeDefinition $node)
+    {
+        $node
+            ->children()
+            ->arrayNode('indexes')
+            ->isRequired()
+            ->requiresAtLeastOneElement()
+            ->useAttributeAsKey('key')
+            ->prototype('scalar')->end()
+            ->end()
+            ->end();
+    }
+    /**
+     * Adding searchd to config
+     * 
+     * @param ArrayNodeDefinition $node the root node
+     * 
+     * @return null
+     */
+    protected function addSearchdSection(ArrayNodeDefinition $node)
+    {
+        $node
+            ->children()
+            ->arrayNode('searchd')
+            ->addDefaultsIfNotSet()
+            ->children()
+            ->scalarNode('host')->defaultValue('localhost')->end()
+            ->scalarNode('port')->defaultValue('9312')->end()
+            ->scalarNode('socket')->defaultNull()->end()
+            ->end()
+            ->end()
+            ->end();
+    }
 }
